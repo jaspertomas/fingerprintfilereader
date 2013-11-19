6a80c079a3a39842d33fe33991a979c180bf27b2
Jasper Tomas <jaspertomas@gmail.com>
2013-11-19 11:16:49 +0800

    building Holiday model

--- src/models/Holidays.java
+++ src/models/Holidays.java
@@ -5,6 +5,8 @@
 package models;
 
 import java.util.ArrayList;
+import utils.fileaccess.BinaryFileReader;
+import utils.fileaccess.BinaryFileWriter;
 
 /**
  *
@@ -23,13 +25,78 @@ public class Holidays {
         return instance;
     }
     //---------------VARIABLES---------------------  
+    private static final String OUTPUT_FILE_NAME = "holidays.dat";
+    
     ArrayList<Holiday> items=new ArrayList<Holiday>();
 
     public ArrayList<Holiday> getItems() {
         return items;
     }
-    public void reset() {
-        items.clear();
+//    public void reset() {
+//        items.clear();
+//    }
+
+    public void save()
+    {
+        BinaryFileWriter writer = new BinaryFileWriter();
+        writer.connectToFile(OUTPUT_FILE_NAME);
+        
+        for(Holiday item:items)
+        {
+            writer.writeString(item.getNickname());
+            writer.writeString(item.getFname());
+            writer.writeString(item.getMname());
+            writer.writeString(item.getLname());
+            writer.writeDouble(item.getMonthlySalary());
+            writer.writeDouble(item.getCola());
+            writer.writeDouble(item.getDeduction());
+        }
+        writer.close(); 
     }
-    
+    public void load()
+    {
+        items.clear();
+        
+        Holiday h;
+                
+        BinaryFileReader reader = new BinaryFileReader();
+        boolean result=reader.connectToFile(OUTPUT_FILE_NAME);
+        
+        if(!result)
+        {
+            //file not found
+            System.out.println("File fingerprint.dat not found");
+            return;
+        }
+        
+        while(reader.notEOF())
+        {
+            h=new Holiday();
+            h.setNickname(reader.readString());
+            if(!reader.notEOF())break;
+            h.setFname(reader.readString());
+            h.setMname(reader.readString());
+            h.setLname(reader.readString());
+            h.setMonthlySalary(reader.readDouble());
+            h.setCola(reader.readDouble());
+            h.setDeduction(reader.readDouble());
+            items.add(h);
+        }
+        reader.close();        
+    }
+
+    //add items that don't already exist in the items array
+    void generateFromStringArray(EmployeeNameList employeenamelist) {
+//        EmployeeList temp=new EmployeeList();
+        
+        //scan employee list for matching nickname; 
+        //if it doesnt exist, add it
+        for(String nickname:employeenamelist)
+        {
+            if(items.getByNickname(nickname)==null)
+            items.add(new Holiday(nickname,"","","",0d,0d,0d));
+        }
+        save();
+    }
+            
 }
--- src/utils/fileaccess/BinaryFileReadWriter0.java
+++ src/utils/fileaccess/BinaryFileReadWriter0.java
@@ -25,202 +25,202 @@ import java.util.logging.Logger;
  * of interactions with the disk.
  */
 public final class BinaryFileReadWriter0 {
-
-    /**
-     * Change these settings before running this class.
-     */
-    private static final String INPUT_FILE_NAME = "/Users/jaspertomas/1.db";
-    private static final String OUTPUT_FILE_NAME = "/Users/jaspertomas/1a.db";
-
-    /**
-     * Run the example.
-     */
-    public static void main(String... aArgs) throws IOException {
-        BinaryFileReadWriter0 test = new BinaryFileReadWriter0();
-
-//        String a = "hello World";
-//        a = padLeft(a, 20);
-        
-        boolean connected=test.connectToFile(OUTPUT_FILE_NAME);
-        if(connected)
-        {
-            System.out.println(test.readString(10));
-            System.out.println(test.readString(10));
-        }
-        test.close();
-//    Integer i=1666661;
-        
-
-    }
-    
-    InputStream input = null;
-    String filename;
-    boolean connectToFile(String filename)
-    {
-        this.filename=filename;
-        try {
-            input = new BufferedInputStream(new FileInputStream(filename));
-            return (input!=null);
-        } catch (FileNotFoundException ex) {
-            log("File not found.");
-            return false;
-        }           
-    }
-    String readString(int length) {
-        try {
-            byte[] bytes = new byte[length];
-            String s="";
-            input.read(bytes);
-            return bytesToStringUTFCustom(bytes);
-        } catch (IOException ex) {
-            log("myReadString: IOException");
-            return null;
-        }
-    }
-    String readInt() {
-        try {
-            byte[] bytes = new byte[4];
-            String s="";
-            input.read(bytes);
-            return bytesToStringUTFCustom(bytes);
-        } catch (IOException ex) {
-            log("myReadString: IOException");
-            return null;
-        }
-    }    
-    
-
-    /**
-     * Read the given binary file, and return its contents as a byte array.
-     */
-//    byte[] read(String aInputFileName) {
-//        log("Reading in binary file named : " + aInputFileName);
-//        File file = new File(aInputFileName);
-//        log("File size: " + file.length());
-//        byte[] result = new byte[(int) file.length()];
-//        try {
-//            InputStream input = null;
-//            try {
-//                int totalBytesRead = 0;
-//                input = new BufferedInputStream(new FileInputStream(file));
-//                while (totalBytesRead < result.length) {
-//                    int bytesRemaining = result.length - totalBytesRead;
-//                    //input.read() returns -1, 0, or more :
-//                    int bytesRead = input.read(result, totalBytesRead, bytesRemaining);
-//                    if (bytesRead > 0) {
-//                        totalBytesRead = totalBytesRead + bytesRead;
-//                    }
-//                }
 //
-//                log("Num bytes read: " + totalBytesRead);
+//    /**
+//     * Change these settings before running this class.
+//     */
+//    private static final String INPUT_FILE_NAME = "/Users/jaspertomas/1.db";
+//    private static final String OUTPUT_FILE_NAME = "/Users/jaspertomas/1a.db";
+//
+//    /**
+//     * Run the example.
+//     */
+//    public static void main(String... aArgs) throws IOException {
+//        BinaryFileReadWriter0 test = new BinaryFileReadWriter0();
+//
+////        String a = "hello World";
+////        a = padLeft(a, 20);
+//        
+//        boolean connected=test.connectToFile(OUTPUT_FILE_NAME);
+//        if(connected)
+//        {
+//            System.out.println(test.readString(10));
+//            System.out.println(test.readString(10));
+//        }
+//        test.close();
+////    Integer i=1666661;
+//        
+//
+//    }
+//    
+//    InputStream input = null;
+//    String filename;
+//    boolean connectToFile(String filename)
+//    {
+//        this.filename=filename;
+//        try {
+//            input = new BufferedInputStream(new FileInputStream(filename));
+//            return (input!=null);
+//        } catch (FileNotFoundException ex) {
+//            log("File not found.");
+//            return false;
+//        }           
+//    }
+//    String readString(int length) {
+//        try {
+//            byte[] bytes = new byte[length];
+//            String s="";
+//            input.read(bytes);
+//            return bytesToStringUTFCustom(bytes);
+//        } catch (IOException ex) {
+//            log("myReadString: IOException");
+//            return null;
+//        }
+//    }
+//    String readInt() {
+//        try {
+//            byte[] bytes = new byte[4];
+//            String s="";
+//            input.read(bytes);
+//            return bytesToStringUTFCustom(bytes);
+//        } catch (IOException ex) {
+//            log("myReadString: IOException");
+//            return null;
+//        }
+//    }    
+//    
+//
+//    /**
+//     * Read the given binary file, and return its contents as a byte array.
+//     */
+////    byte[] read(String aInputFileName) {
+////        log("Reading in binary file named : " + aInputFileName);
+////        File file = new File(aInputFileName);
+////        log("File size: " + file.length());
+////        byte[] result = new byte[(int) file.length()];
+////        try {
+////            InputStream input = null;
+////            try {
+////                int totalBytesRead = 0;
+////                input = new BufferedInputStream(new FileInputStream(file));
+////                while (totalBytesRead < result.length) {
+////                    int bytesRemaining = result.length - totalBytesRead;
+////                    //input.read() returns -1, 0, or more :
+////                    int bytesRead = input.read(result, totalBytesRead, bytesRemaining);
+////                    if (bytesRead > 0) {
+////                        totalBytesRead = totalBytesRead + bytesRead;
+////                    }
+////                }
+////
+////                log("Num bytes read: " + totalBytesRead);
+////            } finally {
+////                log("Closing input stream.");
+////                input.close();
+////            }
+////        } catch (FileNotFoundException ex) {
+////            log("File not found.");
+////        } catch (IOException ex) {
+////            log(ex);
+////        }
+////        return result;
+////    }
+//
+//    /**
+//     * Write a byte array to the given file. Writing binary data is
+//     * significantly simpler than reading it.
+//     */
+//    void write(byte[] aInput, String aOutputFileName) {
+//        log("Writing binary file...");
+//        try {
+//            OutputStream output = null;
+//            try {
+//                output = new BufferedOutputStream(new FileOutputStream(aOutputFileName));
+//                output.write(aInput);
 //            } finally {
-//                log("Closing input stream.");
-//                input.close();
+//                output.close();
 //            }
 //        } catch (FileNotFoundException ex) {
 //            log("File not found.");
 //        } catch (IOException ex) {
 //            log(ex);
 //        }
-//        return result;
 //    }
-
-    /**
-     * Write a byte array to the given file. Writing binary data is
-     * significantly simpler than reading it.
-     */
-    void write(byte[] aInput, String aOutputFileName) {
-        log("Writing binary file...");
-        try {
-            OutputStream output = null;
-            try {
-                output = new BufferedOutputStream(new FileOutputStream(aOutputFileName));
-                output.write(aInput);
-            } finally {
-                output.close();
-            }
-        } catch (FileNotFoundException ex) {
-            log("File not found.");
-        } catch (IOException ex) {
-            log(ex);
-        }
-    }
-
-    /**
-     * Read the given binary file, and return its contents as a byte array.
-     */
-//    byte[] readAlternateImpl(String aInputFileName) {
-//        log("Reading in binary file named : " + aInputFileName);
-//        File file = new File(aInputFileName);
-//        log("File size: " + file.length());
-//        byte[] result = null;
-//        try {
-//            InputStream input = new BufferedInputStream(new FileInputStream(file));
-//            result = readAndClose(input);
-//        } catch (FileNotFoundException ex) {
-//            log(ex);
+//
+//    /**
+//     * Read the given binary file, and return its contents as a byte array.
+//     */
+////    byte[] readAlternateImpl(String aInputFileName) {
+////        log("Reading in binary file named : " + aInputFileName);
+////        File file = new File(aInputFileName);
+////        log("File size: " + file.length());
+////        byte[] result = null;
+////        try {
+////            InputStream input = new BufferedInputStream(new FileInputStream(file));
+////            result = readAndClose(input);
+////        } catch (FileNotFoundException ex) {
+////            log(ex);
+////        }
+////        return result;
+////    }
+//
+//    /**
+//     * Read an input stream, and return it as a byte array. Sometimes the source
+//     * of bytes is an input stream instead of a file. This implementation closes
+//     * aInput after it's read.
+//     */
+////    byte[] readAndClose(InputStream aInput) {
+////        //carries the data from input to output :    
+////        byte[] bucket = new byte[32 * 1024];
+////        ByteArrayOutputStream result = null;
+////        try {
+////            try {
+////                //Use buffering? No. Buffering avoids costly access to disk or network;
+////                //buffering to an in-memory stream makes no sense.
+////                result = new ByteArrayOutputStream(bucket.length);
+////                int bytesRead = 0;
+////                while (bytesRead != -1) {
+////                    //aInput.read() returns -1, 0, or more :
+////                    bytesRead = aInput.read(bucket);
+////                    if (bytesRead > 0) {
+////                        result.write(bucket, 0, bytesRead);
+////                    }
+////                }
+////            } finally {
+////                aInput.close();
+////                //result.close(); this is a no-operation for ByteArrayOutputStream
+////            }
+////        } catch (IOException ex) {
+////            log(ex);
+////        }
+////        return result.toByteArray();
+////    }
+//
+//    private static void log(Object aThing) {
+//        System.out.println(String.valueOf(aThing));
+//    }
+//
+//    public static String padRight(String s, int n) {
+//        return String.format("%1$-" + n + "s", s);
+//    }
+//
+//    public static String padLeft(String s, int n) {
+//        return String.format("%1$" + n + "s", s);
+//    }
+//
+//    public static String bytesToStringUTFCustom(byte[] bytes) {
+//        char[] buffer = new char[bytes.length];// >> 0
+//        for (int i = 0; i < buffer.length; i++) {
+//            int number = Byte.valueOf(bytes[i]).intValue();
+//            buffer[i] = (char) number;
 //        }
-//        return result;
+//        return new String(buffer);
 //    }
-
-    /**
-     * Read an input stream, and return it as a byte array. Sometimes the source
-     * of bytes is an input stream instead of a file. This implementation closes
-     * aInput after it's read.
-     */
-//    byte[] readAndClose(InputStream aInput) {
-//        //carries the data from input to output :    
-//        byte[] bucket = new byte[32 * 1024];
-//        ByteArrayOutputStream result = null;
+//
+//    private void close() {
 //        try {
-//            try {
-//                //Use buffering? No. Buffering avoids costly access to disk or network;
-//                //buffering to an in-memory stream makes no sense.
-//                result = new ByteArrayOutputStream(bucket.length);
-//                int bytesRead = 0;
-//                while (bytesRead != -1) {
-//                    //aInput.read() returns -1, 0, or more :
-//                    bytesRead = aInput.read(bucket);
-//                    if (bytesRead > 0) {
-//                        result.write(bucket, 0, bytesRead);
-//                    }
-//                }
-//            } finally {
-//                aInput.close();
-//                //result.close(); this is a no-operation for ByteArrayOutputStream
-//            }
+//            input.close();
 //        } catch (IOException ex) {
-//            log(ex);
+//            Logger.getLogger(BinaryFileReadWriter0.class.getName()).log(Level.SEVERE, null, ex);
 //        }
-//        return result.toByteArray();
 //    }
-
-    private static void log(Object aThing) {
-        System.out.println(String.valueOf(aThing));
-    }
-
-    public static String padRight(String s, int n) {
-        return String.format("%1$-" + n + "s", s);
-    }
-
-    public static String padLeft(String s, int n) {
-        return String.format("%1$" + n + "s", s);
-    }
-
-    public static String bytesToStringUTFCustom(byte[] bytes) {
-        char[] buffer = new char[bytes.length];// >> 0
-        for (int i = 0; i < buffer.length; i++) {
-            int number = Byte.valueOf(bytes[i]).intValue();
-            buffer[i] = (char) number;
-        }
-        return new String(buffer);
-    }
-
-    private void close() {
-        try {
-            input.close();
-        } catch (IOException ex) {
-            Logger.getLogger(BinaryFileReadWriter0.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
 }
